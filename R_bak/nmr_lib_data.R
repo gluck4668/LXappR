a4425992fea5e1e0ec474652900000bb50cc7834
gluck4668 <109885777+gluck4668@users.noreply.github.com>
2024-11-22 08:41:35 +0800

    Delete R_bak directory

--- R_bak/nmr_lib_data.R
+++
@@ -1,9 +0,0 @@
-library(openxlsx)
-
-lib_nmr_hmdb <- read.csv ("lib_nmr_hmdb.csv")
-nmr_400M <- readRDS("nmr_400M.rds")
-
-usethis::use_data(lib_nmr_hmdb,overwrite = T)
-usethis::use_data(nmr_400M,overwrite = T)
-
-data(lib_nmr_hmdb)
